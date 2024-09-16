40b1952ba552443404ca9b69fac4e36356ce8b00
FRA-SERVER <srajenimbalkar@bedgear.com>
2024-09-16 15:02:32 -0400

    nvim-tree ctrl+n map

--- private_dot_config/nvim/private_init.lua
+++ private_dot_config/nvim/private_init.lua
@@ -391,7 +391,17 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+			-- Set a keybind to toggle nvim-tree
+			vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
+			-- Auto open nvim-tree on startup
+			vim.api.nvim_create_autocmd("VimEnter", {
+				callback = function()
+					if vim.fn.argc() == 0 then
+						require("nvim-tree.api").tree.open()
+					end
+				end,
+			})
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "<leader>/", function()
 				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
